b4506f218fdf1c7738a7277680d3d17aaecfac75
Jiteman <jiteman@gmail.com>
2019-06-26 16:50:48 +0300

    syntax fix

--- control-processing/receiver-table-processing.lua
+++ control-processing/receiver-table-processing.lua
@@ -5,5 +5,5 @@ end
 function Remove_and_get_receiver_from_receiver_table( the_receiver_entity )
 	local the_receiver = global.jitemans_channeled_wireless_signals.receivers[ the_receiver_entity.unit_number ]
 	global.jitemans_channeled_wireless_signals.receivers[ the_receiver_entity.unit_number ] = nil
-	return the_reciever
+	return the_receiver
 end
